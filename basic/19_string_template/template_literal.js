8957655664dfebe6ccaab8612a7db97a5ea3c0d8
Nursalim <nursalim@thebodyshop.co.id>
2022-04-23 11:37:07 +0700

    javascript string literal

---
+++ basic/19_string_template/template_literal.js
@@ -0,0 +1,9 @@
+let firstName = "John";
+let lastName = "Doe";
+let text = `Welcome ${firstName}, ${lastName}`;
+console.log(text);
+
+let price = 10;
+let VAT = 0.25;
+let total = `Total: ${(price * (1 + VAT)).toFixed(2)}`;
+console.log(total);
