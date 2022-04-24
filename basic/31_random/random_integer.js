8e7516fdddcadef2014639f598a7baaa67ef869e
Nursalim <nursalim@thebodyshop.co.id>
2022-04-24 19:54:20 +0700

    javascript random

---
+++ basic/31_random/random_integer.js
@@ -0,0 +1,16 @@
+// Math.random() used with Math.floor() can be used to return random integers.
+
+// Returns a random integer from 0 to 9:
+console.log(Math.floor(Math.random() * 10));
+
+// Returns a random integer from 0 to 10:
+console.log(Math.floor(Math.random() * 11));
+
+// Returns a random integer from 0 to 99:
+console.log(Math.floor(Math.random() * 100));
+
+// Returns a random integer from 1 to 10:
+console.log(Math.floor(Math.random() * 10) + 1);
+
+// Returns a random integer from 1 to 100:
+console.log(Math.floor(Math.random() * 100) + 1);
