75ec79f16024d1ac1bfa7f2ada79169f6c78a096
Nursalim <nursalim@thebodyshop.co.id>
2022-04-24 20:01:29 +0700

    javascript boolean

---
+++ basic/32_boolean/boolean_function.js
@@ -0,0 +1,22 @@
+// You can use the Boolean() function to find out if an expression (or a variable) is true:
+console.log(Boolean(10 > 9));
+
+// or even easier
+console.log(10 > 9);
+
+// True
+console.log(Boolean(100));
+console.log(Boolean(3.14));
+console.log(Boolean(-15));
+console.log(Boolean("Hello"));
+console.log(Boolean("false"));
+console.log(Boolean(7 + 1 + 3.14));
+
+// False
+console.log(Boolean(0));
+console.log(Boolean(-0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(false));
+console.log(Boolean(100 / "Hallo"));
