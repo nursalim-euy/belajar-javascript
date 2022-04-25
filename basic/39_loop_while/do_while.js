a359b13d15ec36ba6e41739678415188fc552a4d
Nursalim <nursalim@thebodyshop.co.id>
2022-04-25 20:45:32 +0700

    javascript loop while

---
+++ basic/39_loop_while/do_while.js
@@ -0,0 +1,14 @@
+// The do while loop is a variant of the while loop. This loop will execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true.
+/*
+syntax:
+do {
+  // code block to be executed
+}
+while (condition);
+
+*/
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < 10);
---
+++ basic/39_loop_while/loop_while.js
@@ -0,0 +1,13 @@
+// The while loop loops through a block of code as long as a specified condition is true.
+/*
+syntax:
+while (condition) {
+  // code block to be executed
+}
+*/
+
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
