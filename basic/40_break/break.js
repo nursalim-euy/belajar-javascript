87e10127e0ee6f770c1f868b7b9a77a965ad6553
Nursalim <nursalim@thebodyshop.co.id>
2022-04-25 20:49:14 +0700

    javascript break

---
+++ basic/40_break/break.js
@@ -0,0 +1,8 @@
+// The break statement "jumps out" of a loop.
+
+for (let i = 0; i < 10; i++) {
+  if (i == 3) {
+    break;
+  }
+  console.log(i);
+}
---
+++ basic/40_break/continue.js
@@ -0,0 +1,7 @@
+// The continue statement "jumps over" one iteration in the loop.
+for (let i = 0; i < 10; i++) {
+  if (i == 3) {
+    continue;
+  }
+  console.log(i);
+}
