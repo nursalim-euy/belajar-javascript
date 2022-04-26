355e52900636b52ab3f9d277058b87ec29da6990
Nursalim <nursalim@thebodyshop.co.id>
2022-04-26 18:30:21 +0700

    javascript typeof

---
+++ basic/43_typeof/type_of_operator.js
@@ -0,0 +1,10 @@
+console.log(typeof "John");
+console.log(typeof 3.14);
+console.log(typeof NaN);
+console.log(typeof false);
+console.log(typeof [1, 2, 3, 4]);
+console.log(typeof { name: "John", age: 34 });
+console.log(typeof new Date());
+console.log(typeof function () {});
+console.log(typeof null);
+console.log(typeof myCar);
