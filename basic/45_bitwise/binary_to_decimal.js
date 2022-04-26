b8488a5386ffc72dcd9405181e74c775a3448028
Nursalim <nursalim@thebodyshop.co.id>
2022-04-26 20:41:51 +0700

    javascript bitwise

---
+++ basic/45_bitwise/binary_to_decimal.js
@@ -0,0 +1,6 @@
+function bin2dec(bin) {
+  return parseInt(bin, 2).toString(10);
+}
+
+console.log(bin2dec(100));
+console.log(bin2dec(111));
---
+++ basic/45_bitwise/bitwise.js
@@ -0,0 +1,7 @@
+console.log(5 & 1);
+console.log(5 | 1);
+console.log(~5);
+console.log(5 << 1);
+console.log(5 ^ 1);
+console.log(5 >> 1);
+console.log(5 >>> 1);
