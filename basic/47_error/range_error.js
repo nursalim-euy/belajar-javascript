d6b998949d3eaae3f5bc6574d4c4c938b2067ca4
Nursalim <nursalim@thebodyshop.co.id>
2022-04-27 20:08:18 +0700

    javascript errors

--- basic/47_error/range_error.js
+++ basic/47_error/range_error.js
@@ -1,5 +1,6 @@
 // A RangeError is thrown if you use a number that is outside the range of legal values.
 // For example: You cannot set the number of significant digits of a number to 500.
+
 let num = 1;
 try {
   num.toPrecision(500);
